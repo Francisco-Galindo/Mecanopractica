500875ad04a5c3c0c10d8f532a0c863bd78e50a2
Francisco-Galindo <paqui10718@gmail.com>
2021-01-16 11:44:36 -0600

    Validando lo que escribe el usuario

--- TouchType/practica/static/practica/words.js
+++ TouchType/practica/static/practica/words.js
@@ -1,12 +1,34 @@
+var total_presses = 0;
+var correct_presses = 0;
+var incorrect_presses = 0;
+var raw_spaces = 0;
+var playing = false;
+var playing_timer = 0;
+var timer = 0;
+var acc;
+var raw_wpm;
 document.addEventListener('DOMContentLoaded', function() {
 
-    // Botones para pedir cada tipo de palabras
-    //document.querySelector('#doscientos').addEventListener('click', () => words('200'));
-    //document.querySelector('#mil').addEventListener('click', () => words('1000'));
-    //document.querySelector('#diez_mil').addEventListener('click', () => words('10000'));
-
     words('1000');
+
+    setInterval(function(){
+        if (playing == true) {
+            timer ++;
+            playing_timer --;
+            if (timer === 60 || playing_timer === 0) {
+                results();
+            }
+            show_vel();
+            console.log(timer, raw_wpm, acc);
+        }
+    }, 1000)
+
+    //Timer para contar el tiempo restante y saber si el usuario esta afk
+
 });
+//Hay que crear una funcion para que inicie el juego, que llame a words y ponga playing en true
+
+// Catalogo de los grupos
 
 function words(words) {
 
@@ -59,18 +81,24 @@ function deleteKey() {
 // La función de escribir debe terminar la prueba cuando el tiempo acabe o (se escriba correctamente la última palabra, o se presione espacio en la última palabra), esto depende del modo en el que este el programa
 function checkKeyPresses(key) {
     // checa solamente la u;tima letra de la forma y comparala con la letra correspondiente en los spans
-    
+    let a = getUnwrittenKey(-1);
     // Línea para evitar que el shift sea detectado de manera incorrecta
     if (key !== "Shift"){
         // Obtener el span con el cual comparar lo que ha sido tecleado
         const other_key = getUnwrittenKey(0);
-        // Si coincide el contenido del span con la tecla oprimida, marcar el span como correcto
+        if (key === " ") {
+            raw_spaces ++;
+        }
+        // Si coincide el contenido del span con la tecla oprimida, marcar el span como correct
         if (key === other_key.innerHTML) {
             other_key.className = "correct";
+            correct_presses ++;
         }
         else {
             other_key.className = "incorrect";
+            incorrect_presses ++;
         }
+        total_presses ++;
     }
     
 }
@@ -85,4 +113,29 @@ function keyPressed(event) {
     else {
         checkKeyPresses(key);
     }
+    playing = true;
+    playing_timer = 15;
+}
+
+// asd
+function calc_vel_acc(){
+    raw_wpm = Math.round((raw_spaces / timer) * 60);
+    acc = Math.round((correct_presses / total_presses) * 100);
+}
+
+function show_vel(){
+    calc_vel_acc()
+    div = document.querySelector('#left_info');
+    div.innerHTML = `acc: ${acc}%, wpm: ${raw_wpm}`
+}
+
+function results() {
+    playing = false;
+    document.querySelector('#text').style.display = 'none';
+    document.querySelector('#form').style.display = 'none';
+    document.querySelector('#results').style.display = 'block';
+    div = document.querySelector('#results');
+    div.innerHTML += `Resultados`;
+    div.innerHTML += `</br> <span style="font-size: 100px;"> wpm: ${raw_wpm}</span>`;
+    div.innerHTML += `</br>acc: ${acc}%`;
 }
